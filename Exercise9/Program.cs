41ada46839d0a3e7505a653445d202bd46aa7fca
johnny-fernandez <145487736+johnny-fernandez@users.noreply.github.com>
2024-03-05 13:17:38 -0500

    exercise9 completed

---
+++ Exercise9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your row width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter how many rows: ");
+            int height = int.Parse(Console.ReadLine());
+
+            //print rows
+            Console.WriteLine();
+            for (int a =1; a <= height; a++)
+            {
+                //print content of the row to the right
+                for (int b =1; b <= width; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
