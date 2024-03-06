5bca0bf1d72ded334129bc156ae7f4b2762fe5b3
johnny-fernandez <johnny.fernandez@s.capregboces.org>
2024-03-06 13:18:29 -0500

    Problem 2 solved

--- Exercise9/Program.cs
+++ Exercise9/Program.cs
@@ -24,6 +24,21 @@ namespace Exercise9
                 Console.WriteLine();
             }
 
+            //Problem 2
+            Console.WriteLine("Enter the MAX number of asterisk in a row: ");
+            int maxAster = Int32.Parse(Console.ReadLine());
+
+            //print the asterisk
+            Console.WriteLine();
+            for (int i = 1; i <= maxAster; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
         }
     }
 }
